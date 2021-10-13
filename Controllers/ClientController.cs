bcc15d5aff3a591015f837504e1f55c8cfcf39d1
Vladimir <wowalion@mail.ru>
2021-10-13 21:48:28 +0300

    13/10/21
    ol

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TechnologiiProg.Controllers
+{
+    [ApiController]
+    [Route("/Client")]
+    public class ClientController : ControllerBase
+    {
+
+        [HttpGet("Zapis")]
+        public string Zapis(string str)
+        {
+            return str; //"Метод записи"
+        }
+
+        [HttpGet("Oplata")]
+        public string Oplata(string str)
+        {
+            return str;// "Метод оплаты"
+        }
+
+
+        [HttpGet("DocumentsCL")]
+        public string DocumentsCL(string str)
+        {
+            return str;// "Метод передачи документов"
+        }
+    }
+}
